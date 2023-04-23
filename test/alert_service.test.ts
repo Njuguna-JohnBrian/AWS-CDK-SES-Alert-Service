f09a41a5abff56509d01cc43cc95862f64b188e9
John Brian <njugunajb96@gmail.com>
2023-04-24 02:04:40 +0300

    Initial commit

---
+++ test/alert_service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AlertService from '../lib/alert_service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/alert_service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AlertService.AlertServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
